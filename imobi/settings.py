6c64fa1e0c884ec9dbb53ff780d76954ff0cecaa
mielesantos <mielesnts@gmail.com>
2022-01-10 22:51:53 -0300

    formatando css

--- imobi/settings.py
+++ imobi/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l5ttdwebr-oouh&@f1#j*)8y27)%)wm%@7=i-dqyht_1mvdj8f'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
