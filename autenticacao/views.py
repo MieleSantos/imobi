c0640bea48e8c38bea9d83bcc58701ef81e12150
mielesantos <mielesnts@gmail.com>
2022-01-10 22:10:15 -0300

    add cadastro usuario

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib .auth.models import User
+
 
 # Create your views here.
+def cadastro(request):
+
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        if len(username.strip()) == 0 or len(email.strip()) == 0 \
+                or len(senha.strip()) == 0:
+            return redirect('/auth/cadastro')
+
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            return redirect('/auth/cadastro')
+
+        try:
+            user = User(
+                username=username,
+                email=email,
+                password=senha
+            )
+            user.save()
+            return redirect("/auth/logar")
+        except Exception:
+            return redirect('/auth/cadastro')
+
+
+def logar(request):
+    return HttpResponse("Logar")
