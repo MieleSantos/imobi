ae38b1a515ecebebec2eb83f595ac1a79dee5664
mielesantos <mielesnts@gmail.com>
2022-01-16 18:14:18 -0300

    add pagina home

--- plataforma/admin.py
+++ plataforma/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import DiasVisita, Horario
+from .models import Cidade, Imagem
+from .models import Visitas, Imovei
 
-# Register your models here.
+
+@admin.register(Imovei)
+class ImoviAdmin(admin.ModelAdmin):
+    list_display = (
+        "rua", "valor", "quartos",
+        "tamanho", "cidade", "tipo"
+    )
+    list_editable = ('valor', 'tipo')
+    list_filter = ('cidade', 'tipo')
+
+
+admin.site.register(DiasVisita)
+admin.site.register(Horario)
+admin.site.register(Imagem)
+admin.site.register(Cidade)
+admin.site.register(Visitas)
