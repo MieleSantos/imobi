bdf0639284aea37b29e429e0b4497c49b40b1436
mielesantos <mielesnts@gmail.com>
2022-01-16 21:06:03 -0300

    adicionando cancelar agendamento

--- plataforma/urls.py
+++ plataforma/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('imovel/<str:id>', views.imovel, name="imovel"),
     path('agendar_visitas', views.agendar_visitas, name="agendar_visitas"),
     path('agendamentos', views.agendamentos, name="agendamentos"),
+    path('cancelar_agendamento/<str:id>',
+         views.cancelar_agendamento, name="cancelar_agendamento")
 ]
--- plataforma/views.py
+++ plataforma/views.py
@@ -61,3 +61,10 @@ def agendar_visitas(request):
 def agendamentos(request):
     visitas = Visitas.objects.filter(usuario=request.user)
     return render(request, "agendamentos.html", {'visitas': visitas})
+
+
+def cancelar_agendamento(request, id):
+    visitas = get_object_or_404(Visitas, id=id)
+    visitas.status = "C"
+    visitas.save()
+    return redirect('/agendamentos')
